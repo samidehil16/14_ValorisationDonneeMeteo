e668df8e9aaeb7ab406e89ddec346e662267db1a
Romain Noyer <36259558+rnoyer@users.noreply.github.com>
2026-03-11 18:36:06 +0100

    Feat/front clean itn UI (#157)
    * feat: delete unused types
    
    * feat: review select bar ui

--- frontend/app/components/ui/itn/selectBar.vue
+++ frontend/app/components/ui/itn/selectBar.vue
@@ -19,60 +19,66 @@ const granularityValues = reactive([
 </script>
 
 <template>
-    <div class="flex gap-6 px-3 py-2 justify-between items-center">
-        <div id="main-filter" class="flex flex-wrap gap-6">
-            <div id="granularity-form" class="flex gap-6">
-                <UFormField label="Granularité" name="granularity">
-                    <USelect
-                        :model-value="granularity"
-                        :items="granularityValues"
-                        name="granularity"
-                        @update:model-value="itnStore.setGranularity"
-                    />
-                </UFormField>
-
-                <DayPicker v-if="granularity === 'day'" />
-                <MonthPicker v-if="granularity === 'month'" />
-                <YearPicker v-if="granularity === 'year'" />
-
-                <USeparator
-                    orientation="vertical"
-                    class="w-px bg-gray-200 self-stretch"
+    <div
+        id="select-bar-wrapper"
+        class="flex flex-wrap gap-6 px-3 py-2 items-center"
+    >
+        <div
+            id="left-side"
+            class="flex flex-wrap gap-6 items-center self-stretch"
+        >
+            <UFormField label="Granularité" name="granularity">
+                <USelect
+                    :model-value="granularity"
+                    :items="granularityValues"
+                    name="granularity"
+                    @update:model-value="itnStore.setGranularity"
                 />
-            </div>
-            <div id="slice-type-form" class="flex gap-6 items-center">
-                <UTooltip
-                    :disabled="granularity !== 'day'"
-                    :disable-closing-trigger="true"
-                    arrow
-                    :delay-duration="0"
-                    text="Changez la Granularité pour activer cette option."
-                    :content="{
-                        align: 'center',
-                        side: 'top',
-                        sideOffset: 8,
-                    }"
-                >
-                    <span>
-                        <USwitch
-                            v-model="sliceTypeSwitchEnabled"
-                            color="neutral"
-                            :disabled="granularity === 'day'"
-                            unchecked-icon="i-lucide-x"
-                            checked-icon="i-lucide-check"
-                            label="Type de moyenne"
-                            :ui="{
-                                root: 'flex-col justify-between text-center items-center',
-                                container: 'my-auto',
-                            }"
-                            @update:model-value="itnStore.turnOffSliceType"
-                        />
-                    </span>
-                </UTooltip>
+            </UFormField>
 
-                <SliceType v-if="sliceTypeSwitchEnabled" />
-            </div>
+            <DayPicker v-if="granularity === 'day'" />
+            <MonthPicker v-if="granularity === 'month'" />
+            <YearPicker v-if="granularity === 'year'" />
+
+            <USeparator
+                orientation="vertical"
+                size="sm"
+                class="bg-gray-200 h-full self-stretch"
+            />
         </div>
-        <ExportMenu />
+
+        <div id="right-side" class="flex flex-1 gap-6 items-center">
+            <UTooltip
+                :disabled="granularity !== 'day'"
+                :disable-closing-trigger="true"
+                arrow
+                :delay-duration="0"
+                text="Changez la Granularité pour activer cette option."
+                :content="{
+                    align: 'center',
+                    side: 'top',
+                    sideOffset: 8,
+                }"
+            >
+                <span>
+                    <USwitch
+                        v-model="sliceTypeSwitchEnabled"
+                        color="neutral"
+                        :disabled="granularity === 'day'"
+                        unchecked-icon="i-lucide-x"
+                        checked-icon="i-lucide-check"
+                        label="Type de moyenne"
+                        :ui="{
+                            root: 'flex-col justify-between text-center items-center',
+                            container: 'my-auto',
+                        }"
+                        @update:model-value="itnStore.turnOffSliceType"
+                    />
+                </span>
+            </UTooltip>
+
+            <SliceType v-if="sliceTypeSwitchEnabled" />
+            <ExportMenu class="ml-auto" />
+        </div>
     </div>
 </template>
